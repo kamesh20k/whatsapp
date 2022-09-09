aec120590199cd92d857a55dfee65d39de1b0dcd
kamesh20k <96664751+kamesh20k@users.noreply.github.com>
2022-09-09 14:49:32 +0530

    heart removed-5thcommit

--- code.py
+++ code.py
@@ -4,7 +4,7 @@
 import itertools, random
 
 # make a deck of cards
-deck = list(itertools.product(range(1,15),['Spade','Heart','Diamond','Club']))
+deck = list(itertools.product(range(1,15),['Spade','Diamond','Club']))
 
 # shuffle the cards
 random.shuffle(deck)
